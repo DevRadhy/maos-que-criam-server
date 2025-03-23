8d5113a1cadbaa2f25a01088cff9a97bf8c0178c
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2025-03-23 13:57:49 -0300

    feat: add cors

--- src/http/app.ts
+++ src/http/app.ts
@@ -1,24 +1,26 @@
-import express, { type Request, type Response} from 'express'
-import 'express-async-errors'
-import { userRouter } from './routes'
+import cors from "cors";
+import express, { type Request, type Response } from "express";
+import "express-async-errors";
+import { userRouter } from "./routes";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use('/user', userRouter)
+app.use("/user", userRouter);
 
 app.use((error: Error, _request: Request, response: Response) => {
   if (error instanceof Error) {
     response.status(400).json({
-      error: error.message
-    })
+      error: error.message,
+    });
   }
 
   response.status(500).json({
-    status: 'error',
-    message: 'Internal Server Error'
-  })
-})
+    status: "error",
+    message: "Internal Server Error",
+  });
+});
 
-export default app
+export default app;
