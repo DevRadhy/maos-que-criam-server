523ec3ed58c75be909e0391142628058e32eb432
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2025-04-12 16:00:48 -0300

    refactor: add update conditions

--- src/services/user/UpdateUserService.ts
+++ src/services/user/UpdateUserService.ts
@@ -19,9 +19,19 @@ export class UpdateUserService {
     const user = new User(props, props.id);
 
     const userExists = await this.userRepository.findById(user.id);
+    const cpfAlreadyExists = await this.userRepository.findById(user.id);
+    const emailAlreadyExists = await this.userRepository.findById(user.id);
 
     if (!userExists) {
       throw new AppError("Usuário não encontrado", 404);
+    }
+
+    if (cpfAlreadyExists && cpfAlreadyExists.cpf !== userExists.cpf) {
+      throw new AppError("CPF já cadastrado", 400);
+    }
+
+    if (emailAlreadyExists && emailAlreadyExists.email !== userExists.email) {
+      throw new AppError("Email já cadastrado", 400);
     }
 
     await this.userRepository.update(user);
